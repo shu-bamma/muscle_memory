c3a0bebf24032fd8bddbc5e60d00842f0a43610d
minoring <minho1350@gmail.com>
2023-12-03 00:54:49 +0900

    implement collect data

--- scripts/collect_data.py
+++ scripts/collect_data.py
@@ -1,22 +1,94 @@
 import argparse
-from SAR_functions import load_manipulation_SAR, SAR_RL
-import os
+from SAR_tutorial_utils import *
 
 
 def main(args):
-    os.environ["CUDA_VISIBLE_DEVICES"] = args.device
     ica, pca, normalizer = load_manipulation_SAR(args.sar_path)
-    # SAR is used to train on a 100-object reorientation task, Reorient100-v0
-    SAR_RL(
-        env_name="myoHandReorient100-v0",
-        policy_name="SAR-RL",
-        timesteps=5e6,
-        seed="0",
-        ica=ica,
-        pca=pca,
-        normalizer=normalizer,
-        phi=0.66,
-        ckpt_path=args.ckpt_path,
+    policy_name = "SAR-RL"
+    env_name = "myoHandReorient100-v0"
+    phi = 0.66
+
+    frames = []
+    env = SynNoSynWrapper(gym.make(env_name), ica, pca, normalizer, phi)
+
+    camera = "front"
+
+    is_solved = []
+    total_steps = 0
+    model = SAC.load(args.ckpt_path, env=env, verbose=1)
+
+    epi = 0
+    pbar = tqdm(total=args.episodes)
+    # for i, __ in enumerate(tqdm(range(args.episodes))):
+    while epi < args.episodes:
+        env.reset()
+
+        vec = VecNormalize.load(
+            args.env_path,
+            DummyVecEnv([lambda: env]),
+        )
+
+        rs = 0
+        done = False
+        solved = False
+
+        observations = []
+        actions = []
+        rewards = []
+        dones = []
+        infos = []
+
+        curr_steps = 0
+        curr_frames = []
+        while not done:
+            curr_steps += 1
+            o = vec.normalize_obs(env.get_obs())
+            a, __ = model.predict(o, deterministic=False)
+            # Note: It does not store scaled action. (As they do in off_policy_algorithm.py)
+
+            if epi % args.video_intervals == 0:
+                frame = env.sim.renderer.render_offscreen(
+                    width=640, height=480, camera_id=camera
+                )
+                curr_frames.append(frame)
+
+            next_o, r, done, info = env.step(a)
+            solved = np.logical_or(solved, info["solved"])
+
+            observations.append(next_o)
+            actions.append(a)
+            rewards.append(r)
+            dones.append(done)
+            infos.append(info)
+
+            rs += r
+
+        if solved:
+            epi += 1
+            total_steps += curr_steps
+            for i in range(len(observations)):
+                model._store_transition(
+                    model.replay_buffer,
+                    np.array([actions[i]]),
+                    np.array([observations[i]]),
+                    np.array([rewards[i]]),
+                    np.array([dones[i]]),
+                    [infos[i]],
+                )
+            frames.extend(curr_frames)
+            pbar.update(1)
+
+        is_solved.append(solved)
+
+    # Save replay buffer.
+    model.save_replay_buffer(f"{policy_name}_buffer_{env_name}_{args.seed}")
+
+    print(f"Average success rate: {np.mean(is_solved)}")
+    print(f"total_steps: {total_steps}")
+
+    env.close()
+    skvideo.io.vwrite(
+        f"collect_data.mp4", np.asarray(frames), outputdict={"-pix_fmt": "yuv420p"}
     )
 
 
@@ -34,6 +106,17 @@ if __name__ == "__main__":
         default="0",
         help="CUDA device number (e.g., '0' or '0,1' for multiple devices)",
     )
-    parser.add_argument("--ckpt_path", type=str, default=None)
+    parser.add_argument("--seed", type=int, default=0)
+    parser.add_argument("--ckpt_path", type=str, required=True)
+    parser.add_argument("--env_path", type=str, required=True)
+    parser.add_argument(
+        "--episodes", type=int, default=30000, help="Number of episodes."
+    )
+    parser.add_argument(
+        "--video_intervals",
+        type=int,
+        default=1000,
+        help="How often to save videos in every episode.",
+    )
     args = parser.parse_args()
     main(args)
